06d47279fb2d75ecdf674ef43ebe88d22920649e
Mohsen khodadadzadeh <Carrier_pigeon2009@yahoo.com>
2016-11-15 11:01:18 +0330

    Push To Git

--- MongooseDeamonSwiftDemo/AppDelegate.swift
+++ MongooseDeamonSwiftDemo/AppDelegate.swift
@@ -10,11 +10,12 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    let mongooseDeamon  = MongooseDaemon()
     var window: UIWindow?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        mongooseDeamon.startMongooseDaemon("8080")
         // Override point for customization after application launch.
         return true
     }
@@ -38,6 +39,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(application: UIApplication) {
+        mongooseDeamon.stopMongooseDaemon()
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
