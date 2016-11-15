3ae8f22f445613caae7c4e9b68589134436189cf
Mohsen khodadadzadeh <Carrier_pigeon2009@yahoo.com>
2016-11-15 10:30:39 +0330

    Initial Commit

---
+++ MongooseDeamonSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MongooseDeamonSwiftDemo
+//
+//  Created by sibapp on 11/15/16.
+//  Copyright © 2016 MKhodadadzadeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
